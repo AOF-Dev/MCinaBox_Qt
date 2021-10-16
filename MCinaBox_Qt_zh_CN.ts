e02e93020ec101beaf99a8c904b9f78e9e89d30a
MCredbear <2363764471@qq.com>
2021-10-17 01:20:28 +0800

    idk

--- MCinaBox_Qt_zh_CN.ts
+++ MCinaBox_Qt_zh_CN.ts
@@ -1,3 +1,123 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="81"/>
+        <source>请选择游戏版本</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="89"/>
+        <source>开始游戏</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="114"/>
+        <source>添加用户</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="134"/>
+        <source>删除用户</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="181"/>
+        <source>游戏设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="226"/>
+        <source>用户名</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="250"/>
+        <source>密码（暂未实现）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="274"/>
+        <source>登录方式（暂未实现）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="293"/>
+        <location filename="main.qml" line="342"/>
+        <source>取消</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="306"/>
+        <location filename="main.qml" line="328"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="324"/>
+        <source>你确定？</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="197"/>
+        <source>添加游戏</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="374"/>
+        <source>还没有游戏，点击添加游戏</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="417"/>
+        <source>当前游戏版本：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="461"/>
+        <location filename="main.qml" line="494"/>
+        <location filename="main.qml" line="543"/>
+        <location filename="main.qml" line="585"/>
+        <location filename="main.qml" line="627"/>
+        <source>返回</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="476"/>
+        <source>获取游戏版本列表失败</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="507"/>
+        <source>快照版</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="517"/>
+        <source>正式版</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="527"/>
+        <source>旧版</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="667"/>
+        <source>安装Optifine（未实现）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="674"/>
+        <source>安装Forge（未实现）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="681"/>
+        <source>安装Fabric（未实现）</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
